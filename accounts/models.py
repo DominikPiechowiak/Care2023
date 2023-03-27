d3f924747e563b51c68c1e58dd5f96f112066030
Dominik <dominik_piechowiak@o2.pl>
2023-03-27 20:51:52 +0200

    Create CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
