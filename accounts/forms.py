ddf6ebcaf0e4c5cf675903b37aba3a8adf359b1f
Dominik <dominik_piechowiak@o2.pl>
2023-03-28 20:24:42 +0200

    test

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email','password1', 'password2')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email','password')
