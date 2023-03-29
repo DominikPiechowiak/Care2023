6dac9d7ccdcd45835c779cdddfe987a30db71bf6
Dominik <dominik_piechowiak@o2.pl>
2023-03-29 17:34:47 +0200

    Add login template

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login, get_user_model
 from .forms import CustomUserCreationForm
 from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
+# from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 
@@ -12,19 +12,20 @@ User = get_user_model()
 class CustomLoginView(LoginView):
     template_name = "login.html"
     fields = "__all__"
-    redirect_authenticated_user = True
+    # redirect_authenticated_user = True
 
 
 class RegisterView(FormView):
     template_name = "register.html"
     form_class = CustomUserCreationForm
-    redirect_authenticated_user = True
-    success_url = reverse_lazy("index")
+    # redirect_authenticated_user = True
+    # success_url = reverse_lazy("index")
 
     def form_valid(self, form):
         user = form.save()
         if user is not None:
             login(self.request, user)
         return super(RegisterView, self).form_valid(form)
+
 
 
