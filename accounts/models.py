c1841e49aed601d3730626af6be6289640f02a97
Jacek <jacek@smejda.pl>
2023-03-29 15:06:06 +0200

    Added model Advertisement

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,14 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
     is_staff = models.BooleanField(default=False)
+
+class Advertisement(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    first_name = CustomUser.first_name
+    last_name = CustomUser.last_name
+    email = CustomUser.email
+    age = models.IntegerField()
+    wykształcenie = models.CharField(max_length=64)
+    experience = models.IntegerField()
+    details = models.TextField()
+    pub_date = models.DateTimeField('date published')
