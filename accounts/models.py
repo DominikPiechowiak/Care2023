72229efa90c1bae0ba90c727e2111ef7eeaae59a
Jacek <jacek@smejda.pl>
2023-04-12 10:28:12 +0200

    Moved Advertisement model from accounts app to viewer app. Maked migrations

--- accounts/models.py
+++ accounts/models.py
@@ -10,14 +10,5 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
 
-class Advertisement(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    first_name = CustomUser.first_name
-    last_name = CustomUser.last_name
-    email = CustomUser.email
-    age = models.IntegerField()
-    education = models.CharField(max_length=64)
-    experience = models.IntegerField()
-    details = models.TextField()
-    pub_date = models.DateTimeField('date published')
+
 
